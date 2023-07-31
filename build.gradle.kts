97e514c0a1ef1eb7ebbe8a65f8ea885ef741af13
M.Mahdi Behroozfar <mmbehroozfar@gmail.com>
2023-07-31 19:55:23 +0330

    Upgraded Gradle dependencies and refactored build.gradle files to KTS

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.47")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
